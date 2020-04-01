7308eaeff455b5162c8f5cb5406370d30512d8f9
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-04-01 16:25:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
-#def square_array(array)
-#  new_array = []
-#    array.each do |element|
-#        new_array << element ** 2
-#    end
-#    new_array
-#end
+def square_array(array)
+  new_array = []
+    array.each do |element|
+        new_array << element ** 2
+    end
+    new_array
+end
 
 #def square_array(numbers)
 # # numbers = [1,2,3]
